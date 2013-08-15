1ed1e6bbc365adaff551206f599c60d548d00a1b
Jon Mosco <jonny.mosco@gmail.com>
2013-08-15 11:06:52 -0400

    validate input

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,11 +3,27 @@
 # == Description
 #
 class iesec (
-  $iesec_admin = 1,
-  $iesec_users = 1,
+  $iesec_admin = true,
+  $iesec_users = true,
 ) {
-  validate_slength($iesec_admin,1)
-  validate_slength($iesec_users,1)
+  if $::operatingsystem != 'Windows' {
+    fail ("Class[iesec] can only be applied to Windows systems. It cannot be used on \"${::operatingsystem}.\"")
+  }
+
+  validate_bool($iesec_admin)
+  validate_bool($iesec_users)
+
+  if $iesec_admin {
+    $iesec_admin = 1
+  } else {
+    $iesec_admin = 0
+  }
+
+  if $iesec_users {
+    $iesec_users = 1
+  } else {
+    $iesec_users = 0
+  }
 
   # Disable IE SEC for Admins
   registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
@@ -20,4 +36,4 @@ class iesec (
     type  => dword,
     data  => $iesec_users,
   }
-}
+  }
